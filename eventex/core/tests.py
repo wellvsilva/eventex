17582a8afc7c45289065d064bc09977a8eb3c07d
Wellington Viana <wellvsilva@gmail.com>
2019-10-22 16:49:11 -0300

    Habilita inscrição

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -13,3 +13,6 @@ class HomeTest(TestCase):
         """Must use index.html"""
         self.assertTemplateUsed(self.response, 'index.html')
 
+    def test_subscription_link(self):
+        self.assertContains(self.response, 'href="/inscricao/"')
+
